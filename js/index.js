9cf43e9635998e39abc2a2079ea25f736ce20e89
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2024-11-04 20:12:31 +0100

    Fix missing semicolon in loadHTML function call for modal initialization

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ checkAuth();
 // Load modals
 const modalsContainer = document.getElementById("modals-container");
 if (modalsContainer) {
-    loadHTML('../modals/createnewpostmodal.html', 'modals-container', initializeCreatePostModal)
+    loadHTML('../modals/createnewpostmodal.html', 'modals-container', initializeCreatePostModal);
     loadHTML('../modals/commentmodal.html', 'modals-container', initializeCommentModal);
 }
 
