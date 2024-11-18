a730c526e162fa5f058ced9a41ebf8a9291c447e
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2024-11-18 19:57:39 +0100

    Add user authentication check in feedProfileFetch function

--- js/API/fetch/fetch.mjs
+++ js/API/fetch/fetch.mjs
@@ -1,10 +1,15 @@
 import { headers } from "../headers.mjs";
+import { loadStorage } from "../../storage/loadstorage.mjs";
 
 // Fetches the profile data from the API
 
 export function feedProfileFetch(url, options = {}) {
-    return fetch(url, {
-        ...options,
-        headers: headers(Boolean(options.body)),
-    });
+    // Load the storage data and check if the user is logged in
+    const storage = loadStorage();
+    if (storage.token) {
+        return fetch(url, {
+            ...options,
+            headers: headers(Boolean(options.body)),
+        });
+    }
 }
